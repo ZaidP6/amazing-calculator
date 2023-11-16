4af8adcd365764ae3921f1bf60d8e1c4d19735bb
Pilar <aguilar.dimar23@triana.salesianos.edu>
2023-11-16 10:41:19 +0100

    Calculadora java

---
+++ IncredibleCalculator.java
@@ -0,0 +1,41 @@
+/**
+ * Calculadora muy básica para aprender a manejar las etiquetas de Git
+ * y los argumentos del método main
+ * 
+ * @author Luis Miguel López Magaña
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        if (args.length < 3) {
+            System.out.println("Sintaxis: java Calculadora.java operando1 operador operando2");
+            // java Calculadora.java 1 + 2
+            return;
+        }
+
+        double n1 = Double.parseDouble(args[0]);
+        double n2 = Double.parseDouble(args[2]);
+        String op = args[1];
+
+        switch (op) {
+            case "+":
+                System.out.println("Resultado: " + (n1 + n2));
+                break;
+            case "-":
+                System.out.println("Resultado: " + (n1 - n2));
+                break;
+            case "*":
+                System.out.println("Resultado: " + (n1 * n2));
+                break;
+            case "/":
+                System.out.println("Resultado: " + (n1 / n2));
+                break;
+            default:
+                System.err.println("Error en el operador introducido");
+
+        }
+
+    }
+
+}
